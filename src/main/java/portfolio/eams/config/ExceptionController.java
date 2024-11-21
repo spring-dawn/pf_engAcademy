13e66684a1f0f21b7c93b2379467e8a20505ae8a
hblee <one_spring_day94@naver.com>
2024-11-21 22:11:51 +0900

    추가-백엔드-전역 예외 처리 초안

---
+++ src/main/java/portfolio/eams/config/ExceptionController.java
@@ -0,0 +1,29 @@
+package portfolio.eams.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import portfolio.eams.dto.ResponseDto;
+import portfolio.eams.util.TimeUtils;
+
+@Slf4j
+@RestControllerAdvice
+public class ExceptionController {
+    /*
+    전역 예외 처리 포맷
+     */
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> defaultServerException(Exception e) {
+        log.error("Exception!", e);
+        ResponseDto defaultRes = new ResponseDto("500", "내부 에러 발생", TimeUtils.timestamp());
+
+        return new ResponseEntity<>(
+                defaultRes,
+                HttpStatus.INTERNAL_SERVER_ERROR  // 500
+        );
+    }
+
+}
---
+++ src/main/java/portfolio/eams/dto/ResponseDto.java
@@ -0,0 +1,5 @@
+package portfolio.eams.dto;
+
+public record ResponseDto(String errCode, String errMsg, String errTime) {
+    // Java 17 Record. 내용이 짧고 부분 사용 여지가 없는 dto 의 경우 이 클래스로 작성 시도.
+}
