2a77c4ab4e409c4eb37aab7c6b4015d54d417e38
Lariatte <one_spring_day94@naver.com>
2025-02-01 15:25:31 +0900

    추가- 노트북용 개발환경 구축, jasypt 테스트코드 등

---
+++ src/test/java/portfolio/eams/config/jasypt/JasyptEncryptorConfigTest.java
@@ -0,0 +1,31 @@
+package portfolio.eams.config.jasypt;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@SpringBootTest
+@ActiveProfiles("notebook")
+class JasyptEncryptorConfigTest {
+
+    // jasypt encryption
+    @Autowired
+    JasyptEncryptorConfig jasypt;
+
+
+    @Test
+    @DisplayName("jasypt 활용")
+    void setJasypt() {
+        String enc = "enc";
+
+        String encrypted = jasypt.encrypt(enc);
+        System.out.println("암호화:"+ encrypted);
+
+        String decrypted = jasypt.decrypt(encrypted);
+        System.out.println("복호화:"+ decrypted);
+    }
+
+}
